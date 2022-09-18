e6f105476bc23c909a56d9781333eabac46835d1
Wilfredo <colmenares1127@gmail.com>
2022-09-18 15:39:39 +0200

    parallelization version01

--- Operators.py
+++ Operators.py
@@ -3,18 +3,28 @@
 
 """
 
+from random import random
 import numpy as np
 import copy
+from mpi4py import MPI
 
 ############ constanst declarations
-NUM_OF_POINTS = 10  # number of points for the discretize grid
+NUM_OF_POINTS = 20  # number of points for the discretize grid
 D_SIZE = 1  # the size of the domain
-
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()  # how many processor there are
+name = comm.Get_name()
+N = 9  # how many columns will i store per processor (num of cols-2)/(num of processors)
 
 ############ class
 class Operator:
     def __init__(self, vec_funct_in_x, vec_funct_in_y) -> None:
-  
+        """
+            vel_funct_in_x : the velocity function for x-coordinates
+            vel_funct_in_y : the velocity function for y-coordinates
+
+        """
         self.delta = D_SIZE / (NUM_OF_POINTS - 1)  # size of the grid
         self.x_size = np.linspace(0, D_SIZE, NUM_OF_POINTS)
         self.y_size = np.linspace(0, D_SIZE, NUM_OF_POINTS)
@@ -28,10 +38,10 @@ class Operator:
     # partial derivatives
     def numerical_partial_deriv_of_x(self, vector_field_fx):  # central difference squeme for x
         delta_x = np.zeros_like(vector_field_fx)
-
+        num_of_col = vector_field_fx.shape[1]
         for j in range(NUM_OF_POINTS):
-            for i in range(NUM_OF_POINTS):
-                if i == NUM_OF_POINTS - 1:  # backward difference
+            for i in range(num_of_col):
+                if i == num_of_col - 1:  # backward difference
                     delta_x[j, i] = (
                         
                     3 * vector_field_fx[j, i] -
@@ -60,12 +70,13 @@ class Operator:
 
         return delta_x
 
+
     def numerical_partial_deriv_of_y(self, vector_field_fy):  # central difference squeme for x
         delta_y = np.zeros_like(vector_field_fy)
-
-        for i in range(NUM_OF_POINTS):
+        num_of_col = vector_field_fy.shape[1]
+        for i in range(num_of_col):
             for j in range(NUM_OF_POINTS):
-                if j == NUM_OF_POINTS - 1:  # backward difference
+                if j == num_of_col - 1:  # backward difference
                     delta_y[j, i] = (
                     3 * vector_field_fy[j, i] -
                     4 * vector_field_fy[j-1, i] +
@@ -93,25 +104,6 @@ class Operator:
 
         return delta_y
     
-    # def numerical_partial_deriv_of_x(self, vector_field_fx):  # central difference squeme for x
-    #     delta_x = np.zeros_like(vector_field_fx)
-
-    #     # [1:-1, 1:-1]  all the points that are not in the boundaries
-    #     delta_x[1:-1, 1:-1] = (vector_field_fx[1:-1, 2:  ] - 
-    #                            vector_field_fx[1:-1, 0:-2] 
-    #                       ) / (                  
-    #                        2*self.delta )
-    #     return delta_x
-
-    # def numerical_partial_deriv_of_y(self, vector_field_fy):  # central difference squeme for y
-    #     delta_y = np.zeros_like(vector_field_fy)
-
-    #     # [1:-1, 1:-1]  all the points that are not in the boundaries
-    #     delta_y[1:-1, 1:-1] = (vector_field_fy[2:  , 1:-1] - 
-    #                            vector_field_fy[0:-2, 1:-1] 
-    #                       ) / (                  
-    #                        2*self.delta )
-    #     return delta_y
 
     # operators
     def numerical_div(self, vector_field_fx, vector_field_fy):
@@ -123,6 +115,7 @@ class Operator:
 
         return partial_x + partial_y
 
+
     def numerical_grad(self, scalar_field):
         """
             takes in a scalar function, return a vector
@@ -131,6 +124,7 @@ class Operator:
         partial_y = self.numerical_partial_deriv_of_y(scalar_field)
 
         return [partial_x,  partial_y]      
+
 
     def laplace(self, f):
         diff = np.zeros_like(f)
@@ -151,22 +145,29 @@ class Operator:
         )
         return diff
 
+
     # jacobi iteration
     def jacobi_iterator(self):
-        phi = np.zeros_like(self.vel_field_x)
-        phi_temp = np.zeros_like(self.vel_field_x)
-        rho = self.numerical_div(self.vel_field_x, self.vel_field_y)
+        """
+            implementation of the jacobi iteration
+        """
+        phi_temp = np.zeros_like(self.vel_field_x)  # allocate space for the temporary answer
+        rho = self.numerical_div(self.vel_field_x, self.vel_field_y)  # source function of poisson equation
         counter = 0
 
         while True:
             phi = np.zeros_like(phi_temp)
+            # we iterate over the internal grid
             for y_dir in range(1, NUM_OF_POINTS - 1, 1):
                 for x_dir in range(1, NUM_OF_POINTS - 1, 1):
+                    # store the values of the stencil
                     up = phi_temp[y_dir-1, x_dir]
                     down = phi_temp[y_dir + 1, x_dir]
                     left = phi_temp[y_dir, x_dir - 1]
                     right = phi_temp[y_dir, x_dir + 1]
                     center = rho[y_dir, x_dir]
+
+                    # calculate answer
                     phi[y_dir, x_dir] = (center * self.delta**2  +
                                         down + 
                                         up +
@@ -176,25 +177,147 @@ class Operator:
             
         
 
-            e1 = np.linalg.norm(phi, ord=np.inf)  # infinity norm
-            e0 = np.linalg.norm(phi_temp, ord=np.inf)
-            error = abs(e0-e1)
+            # e1 = np.linalg.norm(phi, ord=np.inf)  # infinity norm
+            # e0 = np.linalg.norm(phi_temp, ord=np.inf)
+            # error = abs(e0-e1)
+
+            error = abs(phi-phi_temp)
+            error = error.max()
             if error < 2e-10 or counter > 10000:
                 break
 
             counter += 1
 
             phi_temp = copy.deepcopy(phi)
-
-        # Neumann boundary conditions
-        # phi[:, -1] = phi[:, -2]  # condition at the right
-        # phi[0, :] = phi[1, :]  # condition at the top
-        # phi[:, 0] = phi[:, 1] # condition at the left
-        # phi[-1, :] = phi[-2, :]  # condition at the bottom
-
         return phi
 
 
+    # parallel jacobi
+    def p_jacobi_iterator(self):
+            """
+                implementation of the jacobi iteration in parallel
+            """
+            print("hi, i'm: ", rank)
+            counter = 0
+            dimen = self.vel_field_x[:, 0:N+2].shape
+            num_of_p = dimen[1]
+            l_phi_temp = np.zeros(dimen)  # allocate space for the temporary answer in the local processor
+            l_rho = self.numerical_div(self.vel_field_x, 
+                                       self.vel_field_y)[:, rank*N : rank*N + N + 2]  # source function of poisson equation for the local processor
+            
+
+            while True:
+                l_phi = np.zeros_like(l_phi_temp)
+                # we iterate over the internal grid
+                for y_dir in range(1, NUM_OF_POINTS - 1, 1):
+                    for x_dir in range(1, num_of_p-1, 1):
+                        # store the values of the stencil
+                        up = l_phi_temp[y_dir-1, x_dir]
+                        down = l_phi_temp[y_dir + 1, x_dir]
+                        left = l_phi_temp[y_dir, x_dir - 1]
+                        right = l_phi_temp[y_dir, x_dir + 1]
+                        center = l_rho[y_dir, x_dir]
+
+                        # calculate answer
+                        l_phi[y_dir, x_dir] = (center * self.delta**2  +
+                                            down + 
+                                            up +
+                                            left +
+                                            right                                    
+                                            ) * 0.25
+
+                ### we compute the local error
+                l_error = abs(l_phi-l_phi_temp)
+                l_error = l_error.max()
+
+                # we gather the errors to check for the stopping criteria
+                # perform the reduction
+                value_max = np.array(0.0,'d')
+                comm.Reduce(l_error, value_max, op=MPI.MAX, root=0)
+                counter += 1
+                
+
+                # we check for the stopping criteria by sending the error to every processor
+                value_max = comm.bcast(value_max, root=0)
+                if value_max <= 2e-10 or counter > 10000:
+                    print("counter is: ", counter)
+                    break
+                
+
+                # if we need to continue here we create a copy for the next iteration
+                l_phi_temp = copy.deepcopy(l_phi)
+
+                # we exchange the boundaries to compute the next iteration
+                left_boundary = l_phi[:,0]
+                right_boundary = l_phi[:,-2]
+
+                ################################################## we make the sends for left and right boundaries
+                # even processors send and odd ones receive
+                if rank%2 == 0:
+                    #print(f"sending from... {rank} and I'm sending {right_boundary}")
+                    comm.send(l_phi[:,-2], dest=1)
+                    
+                else:
+                    #print(f"I'm {rank} and my current value is {l_phi} i'm suppose to change my left column")
+                    left_boundary = comm.recv(source=0)
+                    l_phi_temp[:,0] = left_boundary
+                    #print(f"\nI'm {rank} and my new left column is: {l_phi_temp}")
+
+                # odd processors send and even ones receive
+                if rank%2 != 0:  # odd processors
+                    #print(f"sending from... {rank} and I'm sending {right_boundary}")
+                    comm.send(l_phi[:,1], dest=0)  # sending left boundary
+                    
+                else:
+                    #print(f"I'm {rank} and my current value is {l_phi} i'm suppose to change my right column")
+                    right_boundary = comm.recv(source=1)
+                    l_phi_temp[:,-1] = right_boundary
+                    #print(f"\nI'm {rank} and my new right column is: {l_phi_temp}")
+
+
+                ### if your are not the first neither the last processor
+                if rank != 0 and rank != size-1:
+                    #print("im here")
+                    # for even processors
+                    if rank%2==0:
+                        comm.send(l_phi[:, 1], dest=rank-1)  # send your left column
+                    else:
+                        right_boundary = comm.recv(source=rank+1)
+                        l_phi_temp[:,-1] = right_boundary
+
+                    # for odd processors
+                    if rank%2!= 0:
+                        comm.send(l_phi[:,-1], dest=rank+1)
+                    else:
+                        left_boundary = comm.recv(source=rank-1)
+                        l_phi_temp[:,1] = left_boundary
+
+            ############################ we gather the result
+            sendbuf = l_phi[1:-1, 1:-1]
+            rows = sendbuf.shape[0]
+            cols = sendbuf.shape[1]
+            dim = rows * cols
+            sendbuf = sendbuf.reshape(dim,1)
+            all_phi = np.array([])
+
+
+            if rank != 0:
+                # every processor should send their data to zero
+                comm.Send(sendbuf, dest=0)
+
+            elif rank == 0:
+
+                all_phi = np.zeros((NUM_OF_POINTS, NUM_OF_POINTS))  # this is the final matrix
+                all_phi[1:-1, rank*N + 1 : rank*N + N + 1] = l_phi[1:-1, 1:-1]  # for the first processor we allocate the result
+
+
+                data = np.empty(dim, dtype='d')  # allocate space to receive the array
+                for each_p in range(1,size):
+                    comm.Recv(data, source=each_p)
+                    all_phi[1:-1, each_p*N + 1 : each_p*N + N + 1] = data.reshape((rows,cols))
+
+                        
+            return (all_phi,rank)
 
 
 ############ testing of the script
--- main.py
+++ main.py
@@ -9,7 +9,12 @@ import matplotlib.pyplot as plt
 import Operators as o
 
 def write_into_csv(arr, name):
-    np.savetxt(f'{name}.csv', arr, delimiter='|')
+    """
+        a function to store results
+        arr: array you want to store
+        name: name of the file you want to save
+    """
+    np.savetxt(f'./data_csv/{name}.csv', arr, delimiter='|')
 
 
 if __name__ == "__main__":
@@ -47,8 +52,7 @@ if __name__ == "__main__":
         return (2-grid_x**2-grid_y**2)*beta
 
 
-
-    # an instance for testing
+    ################################################# an instance for testing
     op = o.Operator(vec_funct_in_x, vec_funct_in_y)
 
     # corrected velocity
@@ -59,16 +63,15 @@ if __name__ == "__main__":
     div_exp = divergence_expected(op.X, op.Y)
 
     # solve the poisson equation
-    phi = op.jacobi_iterator()  # this phi already lost the info around the borders
+    phi = op.jacobi_iterator()
+    phi_ = op.p_jacobi_iterator()  
+    write_into_csv(phi_[0], f'phi_p_{phi_[1]}')
     #phi = op.X**2 + op.Y**2
     laplace_phi = op.laplace(phi)
     div_vel = op.numerical_div(op.vel_field_x, op.vel_field_y)  # compute the divergence of the velocity field
-    numpy_grad = np.gradient(phi, op.delta)
     
-    
-    dif = div_vel + laplace_phi  # first proof
-
-    grad_phi = op.numerical_grad(phi) 
+    dif = div_vel + laplace_phi  # first proof: this must be zero
+    grad_phi = op.numerical_grad(phi)
 
     write_into_csv(phi, 'phi')
     write_into_csv(laplace_phi, 'laplace')
@@ -82,23 +85,11 @@ if __name__ == "__main__":
     #a_ = op.numerical_div(numpy_grad[1], numpy_grad[0])
     #c = -div_vel[2:-2,2:-2] - a
 
-    write_into_csv(a, 'div_grad_phi')
+    
 
     # correction
     new_f_x = op.vel_field_x + grad_phi[0]
     new_f_y = op.vel_field_y + grad_phi[1]
-
-    # boundaries
-    # Neumann boundary conditions
-    # new_f_x[:, -1] = 0  # condition at the right
-    # new_f_x[0, :] = 0 # condition at the top
-    # new_f_x[:, 0] = 0 # condition at the left
-    # new_f_x[-1, :] = 0  # condition at the bottom
-
-    # new_f_y[:, -1] = 0  # condition at the right
-    # new_f_y[0, :] = 0 # condition at the top
-    # new_f_y[:, 0] = 0 # condition at the left
-    # new_f_y[-1, :] = 0  # condition at the bottom
 
     ans = op.numerical_div(new_f_x, new_f_y)
     write_into_csv(ans, 'ans')
@@ -109,7 +100,7 @@ if __name__ == "__main__":
     #plt.quiver(x, y, grad_phi[0], grad_phi[1])
     #plt.show()
     #plt.quiver(x, y, f_modified,f_modified)
-    plt.pcolormesh(op.X, op.Y, ans, vmin=-2, vmax=2)
-    plt.colorbar()
-    plt.show()
-    # print("done")
+    # plt.pcolormesh(op.X, op.Y, ans, vmin=-2, vmax=2)
+    # plt.colorbar()
+    # plt.show()
+    print("done")
